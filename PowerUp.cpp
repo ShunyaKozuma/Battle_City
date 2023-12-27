d869c2677e66982b4303648308a0723eb8a080c8
KevinChu0426 <126142706+KevinChu0426@users.noreply.github.com>
2023-12-28 00:43:20 +0800

    Add files via upload

--- PowerUp.cpp
+++ PowerUp.cpp
@@ -1,6 +1,45 @@
 #include "PowerUp.h"
+#include <QTimer>
 
-PowerUp::PowerUp()
+
+PowerUp::PowerUp(PowerUpType type) : QObject(), QGraphicsPixmapItem()
 {
+    this->type = type;
 
+    setPixmap(pixmapForType(type));
 }
+
+void PowerUp::activate()
+{
+    // Implement the specific effects of each power-up type
+    switch (type) {
+    case Grenade:
+        // Destroy all enemies on the screen
+        // ...
+        break;
+    case Helmet:
+        // Give temporary force field
+        // ...
+        break;
+        // Implement other power-up effects
+    }
+}
+
+QPixmap PowerUp::pixmapForType(PowerUpType type)
+{
+    // Implement logic to return the appropriate pixmap for each power-up type
+    switch (type) {
+    case Grenade:
+        return QPixmap(":/images/Grenade_Item.png".scaled(45,45));
+    case Helmet:
+        return QPixmap(":/images/Helmet_Item.png".scaled(45,45));
+    case Shovel:
+        return QPixmap(":/images/Shovel_Item.png".scaled(45,45));
+    case Star:
+        return QPixmap(":/images/Star_Item.png".scaled(45,45));
+    case Tank:
+        return QPixmap(":/images/Tank_Item.png".scaled(45,45));
+    case Timer:
+        return QPixmap(":/images/Timer_Item.png".scaled(45,45));
+}
+
--- PowerUp.h
+++ PowerUp.h
@@ -1,11 +1,33 @@
 #ifndef POWERUP_H
 #define POWERUP_H
 
+#include <QGraphicsPixmapItem>
+#include <QObject>
 
-class PowerUp
+class PowerUp: public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
+
 public:
-    PowerUp();
+    enum PowerUpType {
+        Grenade,
+        Helmet,
+        Shovel,
+        Star,
+        Tank,
+        Timer
+    };
+
+    PowerUp(PowerUpType type);
+    void activate();
+
+signals:
+    void collected(PowerUpType type);
+
+private:
+    PowerUpType type;
+    QPixmap pixmapForType(PowerUpType type);  // Function to get the appropriate pixmap for each type
+
 };
 
 #endif // POWERUP_H
