5b1aed39344001c2a60b613b67a5bb54c0a9f7ae
Kozuma Shunya <82352273+ShunyaKozuma@users.noreply.github.com>
2024-01-01 22:16:54 +0800

    Add files but have bugs

---
+++ Bullet.h
@@ -0,0 +1,89 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <QGraphicsRectItem>
+#include <QKeyEvent>
+#include <QGraphicsScene>
+#include <QList>
+#include <QRect>
+#include "Player.h"
+#include "Obstruction.h"
+#include "Enemy.h"
+#include "Enemyhealth.h"
+#include "Eagle.h"
+#include "PowerUp.h"
+
+class Bullet : public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+
+public:
+    void handleCollision(QGraphicsItem* item);
+
+public slots:
+    virtual void move()=0;
+
+signals:
+    void bulletHitEnemy(Bullet *bullet, Enemy *enemy);
+    void LosePlayerLife();
+    void bulletHitFlashing();
+    void playerbulletgone();
+
+protected:
+    QTimer *timer;
+};
+
+class Player_Bullet : public Bullet
+{
+    Q_OBJECT
+public:
+    Player_Bullet(Qt::Key keyDirection,int speedbuff,bool powerbuff);
+    ~Player_Bullet();
+    bool getbulletpowerbuff();
+    int setpowerbuff_breakbrick_max();
+    int getpowerbuff_breakbrick_max();
+
+signals:
+
+
+public slots:
+    void move();
+
+private:
+    Qt::Key direction;
+    int bulletspeedbuff;
+    bool bulletpowerbuff;
+    int powerbuff_breakbrick_max=0;
+};
+
+class Enemy_Bullet : public Bullet
+{
+    Q_OBJECT
+    Q_PROPERTY(qreal rotation READ rotation WRITE setRotation )
+public:
+    Enemy_Bullet(int dir,int tank_type,bool helmet);
+    //~Bullet();
+
+    qreal rotation() const;
+    void setRotation(qreal newRotation);
+    bool checkHelmet();
+
+public slots:
+    //void spawn();
+    void move();
+
+signals:
+         //void bullethitsEnemy(Bullet *bullet , Enemy *enemy);
+
+private:
+    int dir;
+    int tank_type;
+    bool helmet;
+    QTimer *timer;
+    qreal m_rotation;
+};
+
+#endif // BULLET_H
